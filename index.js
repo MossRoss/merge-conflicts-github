c0ad59d3a8bd772428934cdcace69f71cf62fede
Mussie <mussierussom@pursuit.org>
2023-05-20 19:18:42 -0400

    Updated the addPersonAsPerson function

---
+++ index.js
@@ -0,0 +1,17 @@
+function createPerson(name, age) {
+  return {
+    name,
+    age,
+    friends: [],
+  };
+}
+
+function addPersonAsFriend(personA, personB) {
+  personA.friends.push(personB);
+  return personA;
+}
+
+module.exports = {
+  createPerson,
+  addPersonAsFriend,
+};
